1bb03bb36a53bb453ed393c941c3b9e698da8311
Austin Gebauer <agebauer@hashicorp.com>
2021-11-06 18:07:52 -0500

    Use python3 as default for use with mininet python API

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ MININET_VERSION="2.3.0"
 
 sudo apt-get update
 sudo apt-get install -y python3 python-is-python3 unzip net-tools
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.5 1
 git clone https://github.com/mininet/mininet
 (cd mininet && git checkout $MININET_VERSION)
 sed -i 's/cgroup-bin/cgroup-tools/g' mininet/util/install.sh
